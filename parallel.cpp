b89e32bec2627433b45e6746d35544fa5d0ed4ad
ElerGard <norfick@mail.ru>
2021-12-23 17:17:20 +1000

    change

--- parallel.cpp
+++ parallel.cpp
@@ -358,51 +358,63 @@ requires (
     ElementType reduce_range(ElementType a, ElementType b, std::size_t n, UnaryFn get, BinaryFn reduce_2, ElementType zero)
 {
     unsigned T = get_num_threads();
-
     struct reduction_partial_result_t
     {
         alignas(std::hardware_destructive_interference_size) ElementType value;
     };
     static auto reduction_partial_results =
-        std::vector<reduction_partial_result_t>(get_num_threads(), reduction_partial_result_t{ zero });
-    constexpr std::size_t k = 2;
-    std::barrier<> bar{ (std::ptrdiff_t)T };
+        std::vector<reduction_partial_result_t>(std::thread::hardware_concurrency(), reduction_partial_result_t{ zero });
 
+    std::barrier<> bar{ T };
+    constexpr std::size_t k = 2;
     auto thread_proc = [=, &bar](unsigned t)
     {
         auto K = ceil_div(n, k);
         double dx = (b - a) / n;
-        std::size_t Mt = K / T, it1 = K % T;
+        std::size_t Mt = K / T;
+        std::size_t it1 = K % T;
+
         if (t < it1)
+        {
             it1 = ++Mt * t;
+        }
         else
-            it1 += Mt * t;
+        {
+            it1 = Mt * t + it1;
+        }
         it1 *= k;
         std::size_t mt = Mt * k;
-        auto it2 = it1 + mt;
+        std::size_t it2 = it1 + mt;
+
         ElementType accum = zero;
-        for (std::size_t i = it1; i < it2; ++i)
+        for (std::size_t i = it1; i < it2; i++)
             accum = reduce_2(accum, get(a + i * dx));
+
         reduction_partial_results[t].value = accum;
-        for (std::size_t s = 1u, s_next = 2u; s < T; s = s_next, s_next += s_next)
+
+        for (std::size_t s = 1, s_next = 2; s < T; s = s_next, s_next += s_next) //TODO assume k = 2
         {
             bar.arrive_and_wait();
-            if ((t % s_next) == 0 && s + t < T)
-                reduction_partial_results[t].value = reduce_2(reduction_partial_results[t].value, reduction_partial_results[t + s].value);
+            if (((t % s_next) == 0) && (t + s < T))
+                reduction_partial_results[t].value = reduce_2(reduction_partial_results[t].value,
+                    reduction_partial_results[t + s].value);
         }
     };
 
     std::vector<std::thread> threads;
-    for (unsigned t = 1; t < T; ++t)
+    for (unsigned t = 1; t < T; t++)
         threads.emplace_back(thread_proc, t);
     thread_proc(0);
     for (auto& thread : threads)
         thread.join();
-
     return reduction_partial_results[0].value;
 }
 
-
+double IntegrateReductionReduceRange(unary_function F, double a, double b)
+{
+    double dx = (b - a) / STEPS;
+    return reduce_range(a, b, STEPS, F, [](auto x, auto y) {return x + y; }, 0.0) * dx;
+}
 
 
 void main()
@@ -425,15 +437,13 @@ void main()
     printf("IntegrateReductionOMP\n");
     ShowExperimentResult(IntegrateReductionOMP);
 
-    printf("IntegrateReductionOMP\n");
+    printf("IntegrateParallelOMP\n");
     ShowExperimentResult(IntegrateParallelOMP);
 
     printf("IntegrateFalseSharingOMP\n");
     ShowExperimentResult(IntegrateFalseSharingOMP);
 
-    /*set_num_threads(4);
-    unsigned V[16];
-    for (unsigned i = 0; i < std::size(V); ++i)
-        V[i] = i + 1;
-    std::cout << "Average: " << reduce_vector(V, std::size(V), [](auto x, auto y) {return x + y; }, 0u) / std::size(V) << '\n';*/
+    printf("IntegrateReductionReduceRange\n");
+    ShowExperimentResult(IntegrateReductionReduceRange);
+
 }
